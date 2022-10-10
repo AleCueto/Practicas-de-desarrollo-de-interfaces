94f55a8dc96b6a759b0d8e5cdb0ef8f24781fff3
AleCueto <alecuetocontacto@gmail.com>
2022-10-10 13:12:01 +0200

    Agregar archivos de proyecto.

---
+++ Practicas-de-desarrollo-de-interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicas_de_desarrollo_de_interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double area;
+
+            Console.Write("Introduce un valor para el radio en metros: ");
+            radio = Convert.ToInt32(Console.ReadLine());
+
+
+            area = Math.PI * Math.Pow(radio, 2);
+
+            Console.WriteLine($"El área del círculo del radio {radio} es {area} metros cuadrados");
+
+        }
+    }
+}
